37639daccb9fc7689f9bf0c434edd0f80a958603
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2022-12-02 01:20:46 +0300

    Modified Smartphone.java file

--- src/main/java/my/idea/list/Smartphone.java
+++ src/main/java/my/idea/list/Smartphone.java
@@ -2,4 +2,10 @@ package my.idea.list;
 
 public class Smartphone extends Product {
 
+    private String manufacturer;
+
+    public Smartphone(int id, String name, int price, String manufacturer) {
+        super(id, name, price);
+        this.manufacturer = manufacturer;
+    }
 }
