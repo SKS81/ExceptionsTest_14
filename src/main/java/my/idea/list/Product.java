b0ec82aec03b0090792bde77034d9b88a09b8395
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2022-12-02 01:17:01 +0300

    Modified Product.java file

--- src/main/java/my/idea/list/Product.java
+++ src/main/java/my/idea/list/Product.java
@@ -2,4 +2,21 @@ package my.idea.list;
 
 public class Product {
 
+    protected int id;
+    protected String name;
+    protected int price;
+
+    public Product(int id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
