12c56428d9efd82ef1c3b7ee6c4835ee7c569c3d
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2022-12-02 01:26:18 +0300

    Modified ProductRepository.java file

--- src/main/java/my/idea/list/ProductRepository.java
+++ src/main/java/my/idea/list/ProductRepository.java
@@ -2,4 +2,44 @@ package my.idea.list;
 
 public class ProductRepository {
 
+    private Product[] items = new Product[0];
+
+    public Product[] findAllProduct() {
+        return items;
+    }
+
+    public Product findById(int id) {
+        for (Product item : items) {
+            if (item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public void deleteById(int id) {
+        if (findById(id) == null) {
+            throw new NotFoundException(
+                    "Товара с таким id не существует:" + id
+            );
+        }
+        Product[] temp = new Product[items.length - 1];
+        int copyToIndex = 0;
+        for (Product item : items) {
+            if (item.getId() != id) {
+                temp[copyToIndex] = item;
+                copyToIndex++;
+            }
+        }
+        items = temp;
+    }
+
+    public void addNewProduct(Product item) {
+        int length = items.length + 1;
+        Product[] temp = new Product[length];
+        System.arraycopy(items, 0, temp, 0, items.length);
+        int lastIndex = temp.length - 1;
+        temp[lastIndex] = item;
+        items = temp;
+    }
 }
