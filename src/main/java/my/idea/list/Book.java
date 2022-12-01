79a15d4b837492db74abe3d2e939d85490763107
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2022-12-02 01:19:01 +0300

    Modified Book.java file

--- src/main/java/my/idea/list/Book.java
+++ src/main/java/my/idea/list/Book.java
@@ -2,4 +2,10 @@ package my.idea.list;
 
 public class Book extends Product {
 
+    private String author;
+
+    public Book(int id, String name, int price, String author) {
+        super(id, name, price);
+        this.author = author;
+    }
 }
